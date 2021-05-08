0f4a12e61b62a25f2fdac6e465d6cfadb0e08823
unknown <guialessio28@gmail.com>
2021-05-08 19:25:06 -0300

    Deploy

---
+++ index.js
@@ -0,0 +1,45 @@
+const titulo = document.getElementById('titulo')
+
+function definirtitulo() {
+    const hora = new Date().getHours()
+    if (hora < 12 && hora > 5) {
+        titulo.innerHTML = "Bom dia!"
+    }
+    else if (hora >= 12 && hora < 18) {
+        titulo.innerHTML = "Boa tarde!"
+    }
+    else if (hora >= 18 || hora < 6) {
+        titulo.innerHTML = "Boa noite!"
+    }
+}
+
+definirtitulo()
+
+function definirTexto() {
+    const horasP = document.getElementById('horas')
+    var horas = new Date().getHours()
+    var minutos = new Date().getMinutes()
+    horasP.innerHTML = "Agora são " + horas + " horas e " + minutos + " minutos."
+}
+
+setInterval(definirTexto, 500)
+
+function youtube(){
+    window.location.href = "https://youtube.com"
+}
+
+function twitter(){
+    window.location.href = "https://twitter.com"
+}
+
+function github(){
+    window.location.href = "https://github.com"
+}
+
+function ulife(){
+    window.location.href = "https://ulife.com"
+}
+
+function multicanais(){
+    window.location.href = "htpps://multicanais.com"
+}
